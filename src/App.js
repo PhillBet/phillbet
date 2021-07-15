6a786d7adc308df930114b8fe8330d4cb51120ab
Phill Betancourt <phill.betan@gmail.com>
2021-07-14 20:28:59 -0500

    Page structuration

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './media/images/PhillLogo.png';
+import './styles/App.css';
+
+import ConsolePane from './components/console_pane/console_pane.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            GitHub Profile.
+          </p>
+          <a
+            className="App-link"
+            href="https://github.com/PhillBet"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            PhillBet
+          </a>
+          <ConsolePane />
+        </header>
+      </div>
+    </>
+
   );
 }
 
---
+++ src/components/console_pane/console_pane.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+//import "../../styles/console_pane.css";
+//import "../../values/text/en/presentation.json"
+const ConsolePane = () => {
+    return (
+        <>
+            <p>
+                My name is Felipe Betancourt Figueroa, I am a Python Backend Developer, I have also been involved in Android mobile application development, Web BackEnd and FrontEnd development (Node, React, JS), Systems and Network administration, among other things.
+            </p>
+        </>
+    );
+};
+
+export default ConsolePane;
